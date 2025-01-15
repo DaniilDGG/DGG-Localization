9e5fb8f8e2ec0afb67f6d72a8f32e48b90735eeb
DaniilDGG <115487836+DaniilDGG@users.noreply.github.com>
2025-01-15 18:41:58 +0300

    Up version to 1.1.0
    * Added option to make LocalizationLoader for Editor/Runtime mode only.
    * Added caching to ProfileLoader.
    * Added the ability to unload added localization. (But it takes longer than adding it, because of the need to rebuild the dictionary and language list.)

--- Loaders/Loader.cs
+++ Loaders/Loader.cs
@@ -4,26 +4,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using DGGLocalization.Data;
 
 namespace DGGLocalization.Loaders
 {
     internal static class Loader
     {
+        #region Fields
+
+        private static readonly List<ILocalizationLoader> RuntimeLoaders = new();
+        private static readonly List<ILocalizationLoader> EditorLoaders = new();
+
+        #endregion
+        
         #region Events
 
         public static event Action<Localization> OnSet;
 
         #endregion
         
-        public static List<(Localization data, string displayName)> LoadLocalizations()
+        public static List<(Localization data, string displayName)> LoadLocalizations(bool isEditor = false)
         {
-            var types = GetLoaders();
+            var loaders = GetLoaders(isEditor);
             var dates = new List<(Localization, string)>();
             
-            foreach (var type in types)
+            foreach (var loader in loaders)
             {
-                var loader = (ILocalizationLoader)Activator.CreateInstance(type);
                 var localizations = loader.GetLocalizationDates();
 
                 foreach (var localization in localizations) dates.Add(localization);
@@ -32,22 +39,34 @@ namespace DGGLocalization.Loaders
             return dates;
         }
 
-        public static void SetLocalization(Localization localization)
+        public static void SetLocalization(Localization localization, bool isRuntime = false)
         {
-            var types = GetLoaders();
+            var loaders = GetLoaders(!isRuntime);
             
-            foreach (var type in types)
+            foreach (var loader in loaders)
             {
-                var loader = (ILocalizationLoader)Activator.CreateInstance(type);
-
                 if (loader.SetLocalizationData(localization)) return;
             }
             
             OnSet?.Invoke(localization);
         }
         
-        private static IEnumerable<Type> GetLoaders() => AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(a => a.GetTypes())
-            .Where(t => t.IsClass && !t.IsAbstract && typeof(ILocalizationLoader).IsAssignableFrom(t) && t.GetCustomAttributes(typeof(LocalizationLoaderAttribute), true).Any());
+        private static IReadOnlyList<ILocalizationLoader> GetLoaders(bool isEditor) => isEditor ? EditorLoaders : RuntimeLoaders;
+
+        static Loader()
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && !t.IsAbstract && typeof(ILocalizationLoader).IsAssignableFrom(t) && t.GetCustomAttributes(typeof(LocalizationLoaderAttribute), true).Any());
+            
+            foreach (var type in types)
+            {
+                var loader = (ILocalizationLoader)Activator.CreateInstance(type);
+                var attribute = type.GetCustomAttribute<LocalizationLoaderAttribute>();
+                
+                if (attribute.RuntimeSupported) RuntimeLoaders.Add(loader);
+                if (attribute.EditorSupported) EditorLoaders.Add(loader);
+            }
+        }
     }
 }
